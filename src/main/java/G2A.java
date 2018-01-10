6612059bb12f53e4f3cd67cf7ab40c73f00a3264
Chris Connolly <chrisc10196@gmail.com>
2018-01-11 12:49:20 +1300

    Retrieving ID from G2A is working

--- src/main/java/G2A.java
+++ src/main/java/G2A.java
@@ -1,19 +1,17 @@
-import com.gargoylesoftware.htmlunit.BrowserVersion;
-import com.gargoylesoftware.htmlunit.Page;
-import com.gargoylesoftware.htmlunit.WebClient;
-import com.gargoylesoftware.htmlunit.html.HtmlPage;
-import com.sun.istack.internal.NotNull;
-import com.sun.org.apache.xerces.internal.impl.xpath.regex.Match;
-import jdk.nashorn.internal.runtime.regexp.joni.Regex;
+import jdk.nashorn.internal.parser.JSONParser;
+import org.apache.commons.io.IOUtils;
+import org.json.JSONArray;
+import org.json.JSONObject;
 import org.jsoup.nodes.Document;
-import org.jsoup.nodes.Element;
 
+import javax.xml.bind.DatatypeConverter;
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-import java.util.logging.Level;
+import java.math.BigInteger;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.nio.charset.Charset;
+import java.nio.charset.StandardCharsets;
+import java.util.*;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -46,15 +44,71 @@ public class G2A {
     }
 
     Game getG2APrice(Game g) {
-        Integer idOfGame = getIDFromLucene();
+        Integer idOfGame = getIDFromLucene(g.getName());
         Integer costOfGame = getCostWithID(idOfGame);
+
+        if (idOfGame == -1 || costOfGame == -1) return null;
 
         g.setCost(costOfGame);
         return g;
     }
 
-    public Integer getIDFromLucene() {
+    public Integer getIDFromLucene(String gameName) {
+        gameName = gameNameModify(gameName);
+        gameName = gameNameToURL_Lucene(gameName);
+        try {
+            JSONObject json = new JSONObject(IOUtils.toString(new URL(gameName), Charset.forName("UTF-8")));
+
+            JSONArray docs = (JSONArray) json.get("docs");
+            JSONObject info = (JSONObject) docs.get(0);
+
+            for (Map.Entry<String, Object> entry : info.toMap().entrySet()) {
+                if (entry.getKey().equalsIgnoreCase("id")) {
+                    return (Integer) entry.getValue();
+                }
+            }
+        }
+        catch (IOException e) {
+            e.printStackTrace();
+        }
         return -1;
+    }
+
+    /**
+     * For G2A, lucene requires that all spaces become '+' and
+     * all special characters get converted to their hexidecimal conversions
+     * to be parsed properly. This method converts a String query to a proper
+     * lucene URL.
+     * @param gameName
+     * @return
+     */
+    private String gameNameToURL_Lucene(String gameName) {
+        StringBuilder url = new StringBuilder(baseIDUrl);
+
+        specialCharsToHex(gameName, url);
+        url = new StringBuilder(url.toString().replaceAll(" ", "+"));
+
+        return url.toString();
+    }
+
+    /**
+     * Alters a String by removing all special characters and converting them to
+     * hexicedimal variants. Used in Lucene to convert string searches to valid inputs
+     * @param gameName
+     * @param url
+     */
+    private void specialCharsToHex(String gameName, StringBuilder url) {
+        Pattern p = Pattern.compile("[^a-z0-9 ]", Pattern.CASE_INSENSITIVE);
+        for (Character c : gameName.toCharArray()) {
+            String toAppend;
+            if (p.matcher(c.toString()).matches()) {
+                toAppend = "%" + toHex(c.toString());
+            }
+            else {
+                toAppend = c.toString();
+            }
+            url.append(toAppend);
+        }
     }
 
     private Integer getCostWithID(Integer idOfGame) {
@@ -67,12 +121,11 @@ public class G2A {
      * @param gameName
      * @return
      */
-    String gameNameCheck(String gameName) {
+    String gameNameModify(String gameName) {
         StringBuilder searchQuery = new StringBuilder(gameName);
         if (!gameName.contains("Global")) {
             searchQuery.append(" Global");
         }
-        System.out.println(searchQueryToUrl(searchQuery.toString()));
         return searchQuery.toString();
     }
 
@@ -109,6 +162,11 @@ public class G2A {
             }
         }
         return nonSpace;
+    }
+
+    public String toHex(String arg) {
+        byte[] bytes = arg.getBytes(StandardCharsets.UTF_8);
+        return DatatypeConverter.printHexBinary(bytes);
     }
 
 }
