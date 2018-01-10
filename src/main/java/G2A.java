7db06f0e45fb957e96240a64243eee12d80f9831
Chris Connolly <chrisc10196@gmail.com>
2018-01-11 10:42:57 +1300

    Found solution to G2A Scraping

--- src/main/java/G2A.java
+++ src/main/java/G2A.java
@@ -1,3 +1,7 @@
+import com.gargoylesoftware.htmlunit.BrowserVersion;
+import com.gargoylesoftware.htmlunit.Page;
+import com.gargoylesoftware.htmlunit.WebClient;
+import com.gargoylesoftware.htmlunit.html.HtmlPage;
 import com.sun.istack.internal.NotNull;
 import com.sun.org.apache.xerces.internal.impl.xpath.regex.Match;
 import jdk.nashorn.internal.runtime.regexp.joni.Regex;
@@ -9,6 +13,7 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.logging.Level;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -19,17 +24,9 @@ import static org.jsoup.Jsoup.connect;
  */
 public class G2A {
 
-    // URL format is baseUrl + platform + endUrl + query (game name converted correctly)
-    public static final String baseUrl = "https://www.g2a.com/?";
+    public static final String baseIDUrl = "https://www.g2a.com/lucene/search/filter?&search="; // This will allow us to get the ID of the game
+    public static final String basePricingURL = "https://www.g2a.com/marketplace/product/auctions/?id="; // Using the ID we can get the minimum price of the game.
     public static String platform = "";
-
-    // Platforms. Incorporate into platform selection for queries
-    public static final String NOPLAT = "";
-    public static final String STEAM = "p=17&";
-    public static final String ORIGIN = "p=16&";
-    public static final String UPLAY = "p=24&";
-
-    public static final String endURL = "search=";
 
     List<Game> toFind;
     Map<Game, Game> returnedResults = new HashMap<>();
@@ -37,57 +34,31 @@ public class G2A {
     public G2A() {}
 
     public G2A(List<Game> toFind) {
-        this.toFind = toFind;
+        this.toFind = new ArrayList<>(toFind);
     }
 
     void getG2APrices() {
         for (Game g: toFind) {
             returnedResults.put(g, getG2APrice(g));
+            break;
         }
         System.out.println(returnedResults);
     }
 
     Game getG2APrice(Game g) {
-        try {
-            Document doc = connect(gameNameToUrl(g.getName())).userAgent("Chrome/63.0.3239").get();
-            boolean productsMatchName = false;
-            for (Element e: doc.getAllElements()) {
-                if (e.id().equals("jq_noproducts")) {
-                    // If the no products div isn't visible, there must be results
-                    String style = e.attr("style"); // getting the inline style element
-                    if (style.contains("none")) {
-                        productsMatchName = true;
-                        break;
-                    }
-                }
-            }
-            if (productsMatchName) {
-                @NotNull Game game = null;
-                Element e = doc.body().select("#tmp_filters_products").first();
-                for (Element child : e.getAllElements()) {
-                    // Found a result, lets only add one result (via most popular for now)
-                    if (child.hasClass("product_name_link")) {
-                        game = new Game();
-                        game.setId(g.getId());
-                        game.setName(e.text());
-                    }
+        Integer idOfGame = getIDFromLucene();
+        Integer costOfGame = getCostWithID(idOfGame);
 
-                    if (child.hasClass("mp-pi-price-min")) {
-                        String value = e.text().replaceAll("[^\\d]", "");
-                        game.setCost(Integer.parseInt(value));
-                        return game;
-                    }
-                }
-            }
-            // No results that matched the game to search against... oh no
-            else {
-                return null;
-            }
-        }
-        catch (IOException e) {
-            e.printStackTrace();
-        }
-        return null;
+        g.setCost(costOfGame);
+        return g;
+    }
+
+    public Integer getIDFromLucene() {
+        return -1;
+    }
+
+    private Integer getCostWithID(Integer idOfGame) {
+        return -1;
     }
 
     /**
@@ -96,25 +67,22 @@ public class G2A {
      * @param gameName
      * @return
      */
-    String gameNameToUrl(String gameName) {
+    String gameNameCheck(String gameName) {
         StringBuilder searchQuery = new StringBuilder(gameName);
         if (!gameName.contains("Global")) {
             searchQuery.append(" Global");
         }
-        //System.out.println(searchQueryToUrl(searchQuery.toString()));
-        return searchQueryToUrl(searchQuery.toString());
+        System.out.println(searchQueryToUrl(searchQuery.toString()));
+        return searchQuery.toString();
     }
 
     private String searchQueryToUrl(String gameName) {
-        StringBuilder toReturn = new StringBuilder(baseUrl + platform + endURL);
+        StringBuilder toReturn = new StringBuilder();
         boolean charAfterSpace = charAfterSpaces(gameName);
 
-        if (charAfterSpace) {
-            toReturn = toReturn.append(gameName.replaceAll(" ", "%20"));
-        }
-        else {
-            toReturn = toReturn.append(gameName.replaceAll(" ", ""));
-        }
+        gameName = gameName.replaceAll(" ", "+");
+        toReturn = toReturn.append(gameName);
+        System.out.println(toReturn.toString());
         return toReturn.toString();
     }
 
@@ -142,4 +110,5 @@ public class G2A {
         }
         return nonSpace;
     }
+
 }
