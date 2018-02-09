11c4cc925077c24115b4a430d8c5c4b0b5de0d9f
Chris Connolly <chrisc10196@gmail.com>
2018-02-09 13:29:38 +1300

    G2A Scrape for multi-threading v0.1

--- experiments/skylar/rate-limit-tester/g2a_rates.py
+++ experiments/skylar/rate-limit-tester/g2a_rates.py
@@ -4,28 +4,35 @@ import queue
 import json
 import ctypes
 import urllib.request
-import sys
+import threading
 
 # Variables for rates of processing
 timeWhenStarted = time.time()
 
-# Overrides user agent (g2a blocks spiders/bots)
-class AppURLopener(urllib.request.FancyURLopener):
-    version = "Mozilla/5.0"
+WORKER_COUNT = 10
 
 '''
 Returns the 'numFound' value via g2a api. This represents the number of pages
 of JSON data that G2A has indexed. Useful for scraping all their data.
 '''
 def getNumListings():
-	opener = AppURLopener()
-	with opener.open('https://www.g2a.com/lucene/search/filter') as fh:
-		response = json.loads(fh.read())
+    try:
+        request = urllib.request.Request("https://www.g2a.com/lucene/search/filter")
+        request.add_header("User-Agent", "Mozilla/5.0")
+        with urllib.request.urlopen(request) as fh:
+            response = json.loads(fh.read())
 
-		if (int(response["numFound"])):
-			return (int(response["numFound"]))
-		else:
-			return 0
+            if (int(response["numFound"])):
+                return (int(response["numFound"]))
+            else:
+                return 0
+
+    except urllib.request.HTTPError:
+        print("HTTP Error")
+        time.sleep(60)
+    except:
+        pass
+
 
 '''
 Returns a JSON formatted list of g2a listings
@@ -35,31 +42,72 @@ When we parse the page with ten listings, this method should be called again
 with a listingPK of 10 to make sure we start where we left off.
 '''
 def fetchG2APage(listingPK):
-	try:
-		opener = AppURLopener()
-		with opener.open("https://www.g2a.com/lucene/search/filter?=&start=%i" % listingPK) as fh:
-			print("Connection accepted - App beginning %i" % listingPK)
-			return json.loads(fh.read())
+    try:
+        request = urllib.request.Request("https://www.g2a.com/lucene/search/filter?=&start=%i" % listingPK)
+        request.add_header("User-Agent", "Mozilla/5.0")
+        with urllib.request.urlopen(request) as fh:
+            return json.loads(fh.read())
 
-	except opener.HTTPError:
-		print("Connection refused")
-	except:
-		print("Unknown error?")
-
+    except urllib.request.HTTPError:
+        print("HTTP Error")
+        time.sleep(60)
+    except:
+        pass
 
 def fetchG2APriceByID(idNum):
-	try:
-		opener = AppURLopener()
-		with opener.open("https://www.g2a.com/marketplace/product/auctions/?id=%i" % idNum) as fh:
-			print("Pricing connection accepted")
-			data = json.loads(fh.read())
-			return data["lowest_price"]
+    try:
+        request = urllib.request.Request("https://www.g2a.com/marketplace/product/auctions/?id=%i" % idNum)
+        request.add_header("User-Agent", "Mozilla/5.0")
+        with urllib.request.urlopen(request) as fh:
+            data = json.loads(fh.read())
+            return float(data["lowest_price"])
 
-	except opener.HTTPError:
-		print("Connection refused")
-	except:
-		print("Unknown error?")
+    except urllib.request.HTTPError:
+        print("HTTP Error")
+        time.sleep(60)
+    except:
+        pass
 
+
+def worker(i, taskQueue):
+    for id in iter(taskQueue.get, None):
+        # mutable stringbuilding
+        l = []
+        l.append("https://www.g2a.com/lucene/search/filter?=&start=")
+        l.append(id)
+        url = ''.join(l)
+
+        pageInfo = fetchG2APage(int(id))
+        listing = pageInfo["docs"][0] # remove numFound, start, docs tags
+        id = int(listing["id"])
+        name = listing["name"]
+
+        price = fetchG2APriceByID(id)
+        print("Game listing with ID of %i costs %.2f" % (id, price))
+
+
+
+
+
+urlQueue = queue.Queue()
+numListings = getNumListings()
+workerList  = []
+
+# Populate queue with ID's for workers to utilise
+for id in range(0, numListings):
+    urlQueue.put("%i" % id)
+
+for i in range(WORKER_COUNT):
+    thread = threading.Thread(target = worker, args = (i, urlQueue))
+    thread.daemon = False
+    thread.start()
+    workerList.append(thread)
+
+# After all tasks have been completed, peacefully terminate the workers
+for i in range(WORKER_COUNT):
+	urlQueue.put(None)
+
+'''
 numListings = getNumListings()
 currListing = 0
 
@@ -77,3 +125,4 @@ while (currListing < numListings):
 		gamesPerPage += 1
 
 	currListing += gamesPerPage
+'''
